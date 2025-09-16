d5f945edf95ce1e5dbec84fe1d22da3e64aa7444
Ewald <hello@christianewald.de>
2025-09-16 17:19:03 +0200

    feat: refactor config and add stylistic or prettier implementation by choice (#90)

---
+++ src/types.ts
@@ -0,0 +1,30 @@
+import type { StylisticCustomizeOptions } from '@stylistic/eslint-plugin'
+
+declare global {
+  export type ConfigTypes = 'prettier' | 'stylistic'
+
+  export type ConfigPropsVue = {
+    vue?: boolean
+    initVuePlugin?: boolean
+  }
+
+  export type ConfigPropsBase = {
+    tailwindcss?: boolean
+    tailwindcssConfig?: Record<string, unknown> & {
+      config?: string
+      customClassProperties?: string[]
+    }
+  } & ConfigPropsVue
+
+  export type ConfigPropsStylistic = {
+    type: 'stylistic'
+    initStylisticPlugin?: boolean
+    stylistic?: StylisticCustomizeOptions
+  }
+
+  export type ConfigPropsPrettier = {
+    type: 'prettier'
+  }
+
+  export type ConfigInput = (ConfigPropsStylistic & ConfigPropsBase) | (ConfigPropsPrettier & ConfigPropsBase)
+}
