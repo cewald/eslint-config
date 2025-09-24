d54073818bae5bf96966adebd02228dd381f8bf7
cewald <hello@christianewald.de>
2025-09-24 06:09:39 +0000

    feat: add prettier-plugin-organize-attributes and update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import config from '@cewald/eslint-config'
 import pluginJs from '@eslint/js'
 import tsEslint from 'typescript-eslint'
-import config from '@cewald/eslint-config'
 
 export default [
   { ignores: ['node_modules', 'dist'] },
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import vuePlugin from 'eslint-plugin-vue'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import vuePlugin from 'eslint-plugin-vue'
 
 export * from './types.js'
 
@@ -17,7 +17,11 @@ export const config = (props: ConfigInput = {}) => {
     confArray.push({
       rules: {
         'vue/block-order': ['error', { order: ['script', 'template', 'style'] }],
+        'vue/block-lang': ['error', { script: { lang: 'ts' } }],
+        'vue/eqeqeq': ['error', 'always'],
         'vue/require-default-prop': 'off',
+        'vue/attributes-order': 'off',
+        'vue/multi-word-component-names': 'off',
       },
     })
   }
